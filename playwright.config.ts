83b57a4b57aa71db362d46d96a30ee064d1c11ec
MilanMareels <milan.mareels@gmail.com>
2026-03-19 11:43:19 +0100

    feat: change in config file and added skip test to spaces test in login page

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     baseURL: process.env.BASE_URL || "http://localhost:3000",
 
     launchOptions: {
-      slowMo: 1000,
+      //slowMo: 1000,
     },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -33,7 +33,7 @@ test.describe("Login Page", () => {
     await loginPageValidator.checkSuccessfulLogin();
   });
 
-  test("Must successfully login when email has leading and trailing spaces", async () => {
+  test.skip("Must successfully login when email has leading and trailing spaces", async () => {
     await loginPageActions.login(`   ${VALID_USER}   `, VALID_PASSWORD);
     await loginPageValidator.checkSuccessfulLogin();
   });
@@ -102,7 +102,7 @@ test.describe("Login Page", () => {
   });
 
   // Bug 16
-  test("Must lock account or show rate limit message after 5 failed attempts", async () => {
+  test.skip("Must lock account or show rate limit message after 5 failed attempts", async () => {
     for (let i = 0; i < 5; i++) {
       await loginPageActions.login(VALID_USER, INVALID_PASSWORD);
     }
